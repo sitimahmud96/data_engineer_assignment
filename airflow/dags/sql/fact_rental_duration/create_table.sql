221bafc99cf29b6aea19f1b236c43b329097844a
Siti Mahmudah <sitimahmud96@gmail.com>
2023-10-15 15:57:46 +0700

    dont use foreign keys on the fact

--- airflow/dags/sql/fact_rental_duration/create_table.sql
+++ airflow/dags/sql/fact_rental_duration/create_table.sql
@@ -4,7 +4,5 @@ CREATE TABLE IF NOT EXISTS fact_rental_duration (
     customer_Key INT,                           
     rental_date DATE,                           
     return_date DATE,                           
-    duration INT,                               
-    FOREIGN KEY (film_key) REFERENCES dim_film(film_key),
-    FOREIGN KEY (customer_Key) REFERENCES dim_customer(customer_key)
+    duration INT
 );
--- airflow/dags/sql/fact_sales/create_table.sql
+++ airflow/dags/sql/fact_sales/create_table.sql
@@ -6,9 +6,5 @@ CREATE TABLE IF NOT EXISTS fact_sales (
     staff_key INT,
     store_key INT,
     rental_id INT,
-    payment_amount DECIMAL(5,2),
-    FOREIGN KEY (customer_key) REFERENCES dim_customer(customer_key),
-    FOREIGN KEY (film_key) REFERENCES dim_film(film_key),
-    FOREIGN KEY (staff_key) REFERENCES dim_staff(staff_key),
-    FOREIGN KEY (store_key) REFERENCES dim_store(store_key)
+    payment_amount DECIMAL(5,2)
 );
